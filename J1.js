c72711f2943a53b333b808f88492cde8bfd2e2c6
DhruvGhevariya <ghevariyadhruv306@gmail.com>
2026-03-16 11:42:21 +0530

    Add files via upload
    lac-1 and lac-2

---
+++ J1.js
@@ -0,0 +1,9 @@
+var a = {
+ "name" : "Test",
+            "age" : 29,
+        }
+    console.log(a.name)
+    console.log(a.age)
+    console.log(a.a)
+    console.log(a)
+    
---
+++ j2.js
@@ -0,0 +1,2 @@
+const data = require("./J1.json");
+console.log(data)
